a0806b024d0cfd155c14c405598c50740fb9f63d
Hip <cnhh85@gmail.com>
2021-04-20 19:18:54 +0700

    add tab logo to html and dummy for navbar

--- utils/dummy.js
+++ utils/dummy.js
@@ -1,3 +1,37 @@
-const DUMMY_NAV_MENU_ITEMS =[
-  
-]
+const DUMMY_NAV_MENU_ITEMS = [
+  {
+    href: "#home",
+    text: "Home",
+    class: "home",
+  },
+  {
+    href: "#intro",
+    text: "My Intro",
+    class: "intro",
+  },
+  {
+    href: "#services",
+    text: "Services",
+    class: "services",
+  },
+  {
+    href: "#portfolio",
+    text: "Portfolio",
+    class: "portfolio",
+  },
+  {
+    href: "#intro",
+    text: "My Intro",
+    class: "intro",
+  },
+  {
+    href: "#testimonials",
+    text: "Testimonials",
+    class: "testimonials",
+  },
+  {
+    href: "#contact",
+    text: "Contact Me",
+    class: "contact",
+  },
+];
